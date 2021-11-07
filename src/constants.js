24c5f2fee5d012fe8ccc33bdbe6fa899ed5473ac
Matthew Duffy <matthewduffy@pop-os.localdomain>
2021-11-07 12:25:42 -0600

    added computerKid photo

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,19 @@
-import StrengthImage from './images/strength.png';
-import hubDesignImage from './images/hubDesign.jpg';
-import hubDrawingImage from './images/hubDrawing.jpg';
-import hubLiveImage from './images/hubLive.png';
-import fiveBookImage from './images/fiveBook.jpg';
-import fiveAppImage from './images/fiveApp.png';
-import fiveSketch from './images/fiveSketch.jpg';
-import alissaDog from './images/alissaDog.jpg';
-import computerDog from './images/computerDog.jpg';
-import timelineChart from './images/timelineChart.jpg';
-import timelineSketch from './images/timelineSketch.jpg';
-import timelineLive from './images/timelineLive.png';
+import StrengthImage from "./images/strength.png";
+import hubDesignImage from "./images/hubDesign.jpg";
+import hubDrawingImage from "./images/hubDrawing.jpg";
+import hubLiveImage from "./images/hubLive.png";
+import fiveBookImage from "./images/fiveBook.jpg";
+import fiveAppImage from "./images/fiveApp.png";
+import fiveSketch from "./images/fiveSketch.jpg";
+import alissaDog from "./images/alissaDog.jpg";
+import computerKid from "./images/computerKid.jpg";
+import timelineChart from "./images/timelineChart.jpg";
+import timelineSketch from "./images/timelineSketch.jpg";
+import timelineLive from "./images/timelineLive.png";
 
 export const aboutData = [
   {
-    title: 'Strength Coach',
+    title: "Strength Coach",
     text: `I've been an avid strength training enthusiast for about 5 years now (with a year-long pause due to COVID-19).
        I worked as a strength coach in Chicago for just over a year. This was my first dip into self-education as I
        learned the necessary material for my training certification. Learning how to learn played a key role in my web development journey, and it started here.
@@ -22,15 +22,15 @@ export const aboutData = [
     picture: StrengthImage,
   },
   {
-    title: 'Computer Enthusiast',
+    title: "Computer Enthusiast",
     text: `There are few things that I am more passionate about than computing. It is my comfort zone. I was privileged to have a father who was keeping up during the digital revolution; 
      he bought his first home computer in the early 80s. In a way, it seemed preordained that I would also become an enthusiast through his influence.
       YouTube has been a major source of entertainment and education throughout my life, especially during this professional transition. I have been an avid PC gamer since the age of 5,
      living vicariously through Pajama Sam is where the interest sparked. My favorites along the way include Diablo 2, Rocket League, and World of Warcraft.`,
-    picture: computerDog,
+    picture: computerKid,
   },
   {
-    title: 'Advocate',
+    title: "Advocate",
     text: `I am constantly searching for ways to play my part in improving the world we share.
      Knowing the lives of others may be positively impacted by my decisions today fuels me to take the necessary actions to ignite change.
       This has manifested in a strong drive to combat global climate change.
@@ -44,28 +44,28 @@ export const aboutData = [
 ];
 export const projectsData = [
   {
-    path: '/projects/the-hub',
-    github: 'https://github.com/mattduff707/the-hub',
-    live: 'https://musing-mirzakhani-92616b.netlify.app',
+    path: "/projects/the-hub",
+    github: "https://github.com/mattduff707/the-hub",
+    live: "https://musing-mirzakhani-92616b.netlify.app",
     showcase: true,
-    title: 'The Hub',
-    subtitle: 'Sci-fi landing page',
+    title: "The Hub",
+    subtitle: "Sci-fi landing page",
     stack: [
-      'React',
-      'Node.js',
-      'Express',
-      'MongoDB',
-      'Redux',
-      'styled components',
-      'React Testing Library',
-      'NASA APOD API',
+      "React",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Redux",
+      "styled components",
+      "React Testing Library",
+      "NASA APOD API",
     ],
     description: `MERN stack browser landing page created to increase my productivity as a developer.
     This is the project I am most proud of and it is in the process of development right now. 
      Task management, NASA's APOD, and a mental math game is the current feature set.`,
     slides: [
       {
-        title: 'The Process',
+        title: "The Process",
         text: `A love for sci-fi and the educational YouTube channel Kurzgesagt – In a Nutshell is what inspired this project.
          The concept of a feature rich browser homepage stems from my desire to take existing ideas and tailor them to my needs to increase my own productivity.
          I sketched a ROUGH design on paper then jumped on Figma to create a simple design.
@@ -74,7 +74,7 @@ export const projectsData = [
         image: hubDrawingImage,
       },
       {
-        title: 'Key Feature',
+        title: "Key Feature",
         text: `I have a weird affinity for mental math, it's a fun challenge and promotes critical focused thought patterns.
          Creating a game for it is a lot easier than continuing to pester my partner for two numbers to multiply.
           With several challenges to tackle, this has been my favorite implementation yet!
@@ -91,18 +91,18 @@ export const projectsData = [
     ],
   },
   {
-    path: '/projects/actor-timeline',
-    github: 'https://github.com/mattduff707/actor-timeline',
-    live: 'https://gracious-kalam-94632e.netlify.app/',
+    path: "/projects/actor-timeline",
+    github: "https://github.com/mattduff707/actor-timeline",
+    live: "https://gracious-kalam-94632e.netlify.app/",
     showcase: true,
-    title: 'Actor Timeline',
+    title: "Actor Timeline",
     subtitle: `Timeline of an actor's movie career`,
-    stack: ['React', 'styled components', 'TMDB API', 'Responsive'],
+    stack: ["React", "styled components", "TMDB API", "Responsive"],
     description: `As a film enthusiast, I had been eager to create an app that utilizes a movie database API.
      This was a fun chance to showcase my proficiency working with a third party API and manipulating the data fetched from it. `,
     slides: [
       {
-        title: 'The Process',
+        title: "The Process",
         text: `Movies are one of my favorite topics of discussion and something I consider to be a hobby of mine.
         So when I decided to create an app that was meant to showcase my proficiency with API's I naturally gravitated toward a movie database.
          I followed my usual process of drawing a rough sketch then creating a simple design on figma.
@@ -112,7 +112,7 @@ export const projectsData = [
         image: timelineSketch,
       },
       {
-        title: 'Key Feature',
+        title: "Key Feature",
         text: `The feature set of this app is pretty straight forward, but I would like to shine a spotlight on the yearly navigation.
          This was achieved by comparing each movie role release date to the previous role's release date when mapping over the array of movie roles.
           Then conditionally render a "year" component if the previous role was released during an earlier year.
@@ -131,26 +131,26 @@ export const projectsData = [
     ],
   },
   {
-    path: '/projects/5-3-1',
-    github: 'https://github.com/mattduff707/5-3-1',
+    path: "/projects/5-3-1",
+    github: "https://github.com/mattduff707/5-3-1",
 
-    title: '5/3/1 Calculator',
+    title: "5/3/1 Calculator",
     showcase: true,
-    live: 'https://focused-mestorf-79f5dc.netlify.app/',
-    subtitle: 'Lifting protocol calculator',
-    stack: ['React', 'styled components'],
+    live: "https://focused-mestorf-79f5dc.netlify.app/",
+    subtitle: "Lifting protocol calculator",
+    stack: ["React", "styled components"],
     description: `A calculator for the famous 5/3/1 lifting program, my personal favorite.
      It was an enjoyable jump into controlled inputs and another app that I use frequently.`,
     slides: [
       {
-        title: 'The Process',
+        title: "The Process",
         text: `I created this app so I could finally have a quick and convenient way to get my lifting numbers each week (Refer to the README for a quick rundown on how the 5/3/1 works if you are interested).
           I focused on trying to make this as dynamic and scalable as I could through the use of functional programming principles.
          `,
         image: fiveBookImage,
       },
       {
-        title: 'Key Feature',
+        title: "Key Feature",
         text: `This app has controlled inputs utilizing Reacts useState hook. On each input is a function that ensures only numbers can be entered into the inputs.
          This was achieved by creating multiple smaller conditional functions that combine to prevent an invalid input value.`,
         image: fiveSketch,
@@ -165,23 +165,23 @@ export const projectsData = [
     ],
   },
   {
-    path: '/projects/portfolio',
-    github: 'https://github.com/mattduff707/my-portfolio',
+    path: "/projects/portfolio",
+    github: "https://github.com/mattduff707/my-portfolio",
     showcase: false,
-    title: 'Portfolio',
-    subtitle: 'This darling portfolio',
-    stack: ['React', 'Framer Motion', 'styled components', 'Responsive'],
+    title: "Portfolio",
+    subtitle: "This darling portfolio",
+    stack: ["React", "Framer Motion", "styled components", "Responsive"],
     description: `Design isn't what I strive to do, but I do enjoy injecting my own creativity into my personal projects. My portfolio is no exception. I hope you like gradients and icons.`,
     slides: false,
   },
 
   {
-    path: '/projects/duffy-web-brain',
-    github: 'https://github.com/mattduff707/matt-blog',
-    live: 'https://www.duffy-web-brain.com/posts',
-    title: 'Duffy Web Brain',
-    subtitle: 'Personal notes app',
-    stack: ['React', 'Gatsbyjs', 'PrismJs'],
+    path: "/projects/duffy-web-brain",
+    github: "https://github.com/mattduff707/matt-blog",
+    live: "https://www.duffy-web-brain.com/posts",
+    title: "Duffy Web Brain",
+    subtitle: "Personal notes app",
+    stack: ["React", "Gatsbyjs", "PrismJs"],
     description:
       "Throughout most of my self education I had note been utilizing any solid not taking methodology. Learning in the IDE wasn't cutting it, so I made my own solution!",
     slides: false,
